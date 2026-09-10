927d3fab2ef6393d816489ab61b0dec1bc3c0c01
nayeem2208 <nayeem2281998@gmail.com>
2026-09-10 14:47:15 +0530

    tavily brandfetch clearout search to test

--- src/routes/companyRoutes.js
+++ src/routes/companyRoutes.js
@@ -5,7 +5,7 @@ import companyController from '../controllers/companyController.js';
 const router = express.Router();
 
 router.get('/resolve', companyController.resolve);
-router.get('/all', companyController.getAll);
-router.get('/:id', companyController.getById);
+// router.get('/all', companyController.getAll);
+// router.get('/:id', companyController.getById);
 
 export default router;
